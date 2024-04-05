ae8aaffb610a049bc954ace302c6dc6895539a40
Eric&TheWeb <43747486+Ericnsamba@users.noreply.github.com>
2024-04-05 15:03:46 +0100

    .

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import SettingsForm from './components/SettingsForm.js';
+import Game from './components/Game.js';
+
+export default class App extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            boardSize: 3,
+            clock: false,
+            time: 10,
+            matchID: 0
+        };
+
+        this.newGame = this.newGame.bind(this);
+    }
+
+    newGame(size, clock, time)
+    {
+        // console.log('New size is ' + size);
+        this.setState((prevState, props) => ({
+            boardSize: size,
+            clock: clock,
+            time: time,
+            matchID: prevState.matchID+1
+        }));
+    }
+
+    render()
+    {
+        return (
+            <div className="app">
+                <SettingsForm defaultValues={this.state} submitCallback={this.newGame} /><br/>
+                <Game key={this.state.matchID}
+                    size={this.state.boardSize}
+                    clock={this.state.clock}
+                    time={this.state.time}
+                    renderInfo={true} />
+            </div>
+        );
+    }
+}
---
+++ src/components/Board.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Square from './Square.js';
+import generateGridNxN from '../util/GameUtil.js';
+
+export default class Board extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.renderSquare = this.renderSquare.bind(this);
+    }
+
+    renderSquare(i)
+    {
+        return (
+            <Square key={i}
+                value={this.props.squares[i]}
+                winner={this.props.winner}
+                clickable={this.props.clickable}
+                onClick={() => this.props.onClick(i)}
+            />
+        );
+    }
+
+    render()
+    {
+        return generateGridNxN(
+            'board',
+            this.props.size,
+            this.renderSquare
+        );
+    }
+}
